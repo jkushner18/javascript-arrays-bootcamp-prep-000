f797a225297e5d4c31f255a31c39f5290295863c
jkushner18@gmail.com Kushner <jkushner18@gmail.com>
2018-01-19 18:38:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,15 @@
 choclateBars =["snickers", "hundred grand","kitkat", "skittles"]
 
+function addElementtoBeginningofArray(arr,ele){
+  
+arr.unshift(ele)
+
+return arr
+  
+}
+
+function destructivelyAddElementtoBeginningofArray(arr, ele){
+  
+(ele,...arr)
+  
+}
