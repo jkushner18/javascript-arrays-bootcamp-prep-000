8bf8986b4fa0e083df6a965888d77ce6a9af06c7
jkushner18@gmail.com Kushner <jkushner18@gmail.com>
2018-01-19 18:42:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,17 +2,17 @@ chocolateBars =["snickers", "hundred grand","kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array,element){
   
-var yup = [ele, ...arr];
+var yup = [element, ...array];
 
 return yup
   
 }
 
-function destructivelyAddElementToBeginningOfArray(arr, ele){
+function destructivelyAddElementToBeginningOfArray(array, element){
   
 arr.unshift(element)
 
-return arr
+return array
 
 
 }
