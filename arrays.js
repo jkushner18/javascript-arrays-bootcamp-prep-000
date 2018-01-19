9201a2ca8ae51b7a213fec879c662f164a14cd1b
jkushner18@gmail.com Kushner <jkushner18@gmail.com>
2018-01-19 18:46:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-chocolateBars =["snickers", "hundred grand","kitkat", "skittles"]
+chocolateBars = ["snickers", "hundred grand","kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array,element){
   
