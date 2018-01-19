67d251553abee1d82a56720ea6f447d822f4ce35
jkushner18@gmail.com Kushner <jkushner18@gmail.com>
2018-01-19 18:41:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,9 @@ function addElementToBeginningOfArray(array,element){
 
 function destructivelyAddElementToBeginningOfArray(arr, ele){
   
+arr.unshift(element)
 
+return arr
 
 
 var yup = [ele, ...arr];
